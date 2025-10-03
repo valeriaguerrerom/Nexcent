d44048c8d622ddf2a21ec032fa0864292711a388
Valeria Guerrero <valeriaso.guerrero@umariana.edu.co>
2025-10-03 10:46:35 -0500

    FEAT

--- src/App/Routes/AppRoutes.jsx
+++ src/App/Routes/AppRoutes.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
-import Home from '../../components/home/Home';
+import Home from '../../components/Home/Home';
 import Blog from '../../components/Blog/Blog';
 import Community from '../../components/Community/Community';
 import Features from '../../components/Features/Features';
